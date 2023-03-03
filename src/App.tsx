ac429d24d144262c74b9eda6dda9cb9695cfc2a0
Mbetania <betaniamarianag@gmail.com>
2023-03-03 18:24:23 -0300

    first commint

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import { Todos } from './components/Todos'
+import { type TodoId, type Todo as TodoType } from './types'
+
+const mockTodos = [
+  {
+    id: '1',
+    title: 'Acariciar a Cane',
+    completed: true
+  },
+  {
+    id: '2',
+    title: 'Volverme loca con ts',
+    completed: false
+  },
+  {
+    id: '3',
+    title: 'Fumar fasito',
+    completed: false
+  }
+]
+
+const App = (): JSX.Element => {
+  const [todos, setTodos] = useState(mockTodos)
+
+  const handleRemove = ({ id }: TodoId): void => {
+    const newTodos = todos.filter(todo => todo.id !== id)
+    setTodos(newTodos)
+  }
+  const handleCompleted = (
+    { id, completed }: Pick<TodoType, 'id' | 'completed'>
+  ): void => {
+    const newTodos = todos.map(todo => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed
+        }
+      }
+      return todo
+    })
+    setTodos(newTodos)
+  }
+  return (
+    <div className='todoapp'>
+      <Todos
+        onToggleCompleteTodo={handleCompleted}
+        onRemoveTodo ={handleRemove}
+         todos={todos}
+      />
+    </div>
+  )
+}
+
+export default App
